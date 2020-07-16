9dc47daefa0462e8e5c077b7877f9b70f19abd66
Henry Jang <hrjang0916@gmail.com>
2020-07-16 14:08:31 -0700

    Add ClientSessionCache to enable session tickets

--- loader/client.go
+++ loader/client.go
@@ -55,12 +55,15 @@ func client(disableCompression, disableKeepAlive, skipVerify bool, timeoutms int
 
 	clientCertPool := x509.NewCertPool()
 	clientCertPool.AppendCertsFromPEM(clientCACert)
+	
+	clientSessionCache := tls.NewLRuClientSessionCache(0)
 
 	tlsConfig := &tls.Config{
 		Certificates:       []tls.Certificate{cert},
 		RootCAs:            clientCertPool,
 		InsecureSkipVerify: skipVerify,
 		SessionTicketsDisabled: false,
+		ClientSessionCache: clientSessionCache,
 	}
 
 	tlsConfig.BuildNameToCertificate()
